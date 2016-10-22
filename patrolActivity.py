a62bbe622a84f69aab6afbf39df1ed95ff6c3859
Martin Urbanec <martin.urbanec@wikimedia.cz>
2016-10-22 21:51:41 +0200

    Enable the tool everywhere

--- patrolActivity.py
+++ patrolActivity.py
@@ -1,12 +1,42 @@
 #!/usr/bin/env python
 #-*- coding: utf-8 -*-
 
+import os
+import cgi
 import HTML
 from datetime import datetime, timedelta
 from wmflabs import db
-conn = db.connect('cswiki')
+
+if 'QUERY_STRING' in os.environ:
+        QS = os.environ['QUERY_STRING']
+        qs = cgi.parse_qs(QS)
+        try:
+                lang = qs['lang'][0]
+        except:
+                lang = "cs"
+        try:
+                family = qs['family'][0]
+        except:
+                family = 'wiki'
+else:
+        lang = "cs"
+        family = "wiki"
+
+wiki = lang + family
+conn = db.connect(wiki)
 
 d = datetime.today() - timedelta(days=100)
+
+print 'Content-type: text/html\n'
+print """
+<!DOCTYPE html>
+<html lang="cs-cz">
+        <head>
+                <meta charset="utf-8" />
+                <title>Statistika patrolářů v """ + wiki + """</title>
+        </head>
+        <body>
+"""
 
 cur = conn.cursor()
 with cur:
@@ -15,3 +45,8 @@ with cur:
 	data = cur.fetchall()
 
 print HTML.table(data)
+
+print """
+</body>
+</html>
+"""
