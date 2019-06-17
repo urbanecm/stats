f3aeffec8e1ba2132c9d423530de1a415f20b784
Martin Urbanec <martin@urbanec.cz>
2019-06-17 22:27:11 +0200

    Add patrol

--- src/app.py
+++ src/app.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from datetime import datetime, timedelta
+import toolforge
 
 app = Flask(__name__, static_folder='../static')
 
@@ -8,7 +10,28 @@ app = Flask(__name__, static_folder='../static')
 def index():
 	return 'a'
 
+@app.route('/patrol')
+def patrol():
+	days = int(request.GET.get('days', 0))
+	lang = request.GET.get('lang', 'cs')
+	family = request.GET.get('family', 'wikipedia')
+	dbname = lang + family.replace('wikipedia', 'wiki')
+	conn = toolforge.connect(dbname)
 
+	d = datetime.today() - timedelta(days=int(days))
+
+	cur = conn.cursor()
+	with cur:
+		sql = 'select count(*), log_user_text from logging where log_type="patrol" and log_action="patrol" and log_timestamp>="' + '{:%Y%m%d%H%M%S}'.format(d) +'" group by log_user order by count(*) desc;'
+		cur.execute(sql)
+		data = cur.fetchall()
+	cur = conn.cursor()
+	with cur:
+		sql = 'select count(*) from logging where log_type="patrol" and log_action="patrol" and log_timestamp>="' + '{:%Y%m%d%H%M%S}'.format(d) +'";'
+		cur.execute(sql)
+		data.append((cur.fetchall()[0][0], "<strong>Celkem</strong>"))
+	
+	return render_template('data.html', data=data, days=days, wiki=dbname)
 
 if __name__ == "__main__":
 	app.run(host="0.0.0.0", port=5000)
