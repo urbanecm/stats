cb7fe8ebbba46f431e92808ae57ced1cda26bc2b
Martin Urbanec <martin@urbanec.cz>
2019-06-17 23:22:33 +0200

    Refactor

--- src/app.py
+++ src/app.py
@@ -6,6 +6,16 @@ import toolforge
 
 app = Flask(__name__, static_folder='../static')
 
+def decode_if_necessary(data):
+	for row in rows:
+		row_ = []
+		for item in row:
+			if isinstance(item, bytes):
+				row_.append(item.decode('utf-8'))
+			else:
+				row_.append(item)
+	return date
+
 @app.route('/')
 def index():
 	return 'a'
@@ -25,14 +35,7 @@ def patrol():
 	with cur:
 		sql = 'select count(*), actor_name from logging join actor on actor_id=log_actor where log_type="patrol" and log_action="patrol" and log_timestamp>="' + '{:%Y%m%d%H%M%S}'.format(d) +'" group by log_actor order by count(*) desc;'
 		cur.execute(sql)
-		rows = cur.fetchall()
-		for row in rows:
-			row_ = []
-			for item in row:
-				if isinstance(item, bytes):
-					row_.append(item.decode('utf-8'))¨
-				else:
-					row_.append(item)
+		rows = decode_if_necessary(cur.fetchall())
 	cur = conn.cursor()
 	with cur:
 		sql = 'select count(*) from logging where log_type="patrol" and log_action="patrol" and log_timestamp>="' + '{:%Y%m%d%H%M%S}'.format(d) +'";'
