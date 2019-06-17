166752ce5dbd86267750b933b434e21bcdaecd89
Martin Urbanec <martin@urbanec.cz>
2019-06-17 23:24:18 +0200

    Fix_refactor

--- src/app.py
+++ src/app.py
@@ -7,14 +7,16 @@ import toolforge
 app = Flask(__name__, static_folder='../static')
 
 def decode_if_necessary(data):
-	for row in rows:
+	data_ = []
+	for row in data:
 		row_ = []
 		for item in row:
 			if isinstance(item, bytes):
 				row_.append(item.decode('utf-8'))
 			else:
 				row_.append(item)
-	return date
+		data_.append(row_)
+	return data_
 
 @app.route('/')
 def index():
@@ -35,12 +37,12 @@ def patrol():
 	with cur:
 		sql = 'select count(*), actor_name from logging join actor on actor_id=log_actor where log_type="patrol" and log_action="patrol" and log_timestamp>="' + '{:%Y%m%d%H%M%S}'.format(d) +'" group by log_actor order by count(*) desc;'
 		cur.execute(sql)
-		rows = decode_if_necessary(cur.fetchall())
+		data = decode_if_necessary(cur.fetchall())
 	cur = conn.cursor()
 	with cur:
 		sql = 'select count(*) from logging where log_type="patrol" and log_action="patrol" and log_timestamp>="' + '{:%Y%m%d%H%M%S}'.format(d) +'";'
 		cur.execute(sql)
-		rows = (cur.fetchall()[0][0], "<strong>Celkem</strong>")
+		data.append((cur.fetchall()[0][0], "<strong>Celkem</strong>"))
 	
 	return render_template('data.html', data=data, days=days, wiki=dbname)
 
